9c80b8cf4a97679e4f9b10eb9026b88d37ad63c6
ilhamwara <ilhamwara@gmail.com>
2017-06-21 10:35:33 +0700

    as

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -182,8 +182,9 @@
                 <li><a href="{{url('karyawan')}}">Data Karyawan<span style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-user"></span></a></li>
                 <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Kompetensi<span  style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-caret-square-o-down"></span></a>
                     <ul class="dropdown-menu">
-                      <li><a href="{{url('kompetensi')}}">Kompetensi<span style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-list-alt"></span></a></li>
-                      <li><a href="{{url('add/jenis-kompetensi')}}">Jenis Kompetensi<span style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-drivers-license-o"></span></a></li>
+                      <li><a href="{{url('kompetensi')}}">Kompetensi<span style="font-size:16px; padding: 5px 0px;" class="pull-right hidden-xs showopacity fa fa-list-alt"></span></a></li>
+                      <li><a href="{{url('tambah-kompetensi')}}">Tambah Kompetensi<span style="font-size:16px; padding: 5px 0px;" class="pull-right hidden-xs showopacity fa fa-list-alt"></span></a></li>
+                      {{-- <li><a href="{{url('add/jenis-kompetensi')}}">Jenis Kompetensi<span style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-drivers-license-o"></span></a></li> --}}
                   </ul>
               </li>
               <li ><a href={{url('pcr')}}>PCR<span style="font-size:16px;" class="pull-right hidden-xs showopacity fa fa-pencil-square-o"></span></a></li>
