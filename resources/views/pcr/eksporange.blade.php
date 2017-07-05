0a54b1d7447223c8b9b2b406a863aa4328e406de
sofyan <sofyansyah.ss@gmail.com>
2017-07-05 22:49:43 +0700

    'asd'

--- resources/views/pcr/eksporange.blade.php
+++ resources/views/pcr/eksporange.blade.php
@@ -169,5 +169,19 @@
 </div>
 @endsection
 @section('javascript')
-<script src="{{asset('js/export.js')}}"></script>
+<script>
+    $(function () 
+    {
+        var kar = $('.pcr_karyawan');
+        var table_html = $('.pcr_karyawan')[0].outerHTML;
+        var css_html = '<style>.export, .text-center, .text-left {border: 1pt solid #333} </style>';
+        $("#btnExport").click(function(e) 
+        {
+            e.preventDefault();
+            window.open('data:application/vnd.ms-excel,' + 
+                encodeURIComponent('<html><head>' + css_html + '</' + 'head><body>' + table_html + '</body></html>'));
+        });
+    });
+
+</script>
 @endsection
